a9ce74741aaa6ff8a23c30cc049c5072a45b54aa
ganesh@enerlly.com <ganesh@enerlly.com>
2020-11-16 16:43:05 +0530

    Added recipe api feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -35,7 +35,20 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     serializer_class = serializers.IngredientSerializer
 
 
-# class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+
+
+# class TagViewSet(viewsets.GenericViewSet,
+#                  mixins.ListModelMixin, mixins.CreateModelMixin):
 #     """Manage tags in the database"""
 #     authentication_classes = (TokenAuthentication,)
 #     permission_classes = (IsAuthenticated,)
@@ -51,7 +64,8 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 #         serializer.save(user=self.request.user)
 #
 #
-# class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+# class IngredientViewSet(viewsets.GenericViewSet,
+#                         mixins.ListModelMixin, mixins.CreateModelMixin):
 #     """Manage ingredients in the database"""
 #     authentication_classes = (TokenAuthentication,)
 #     permission_classes = (IsAuthenticated,)
